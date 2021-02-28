4e32c6e18f9c9d3beb371871ae7df62c72323a0b
Jakob Hoffmann <jfb-hoffmann@hotmail.de>
2021-02-28 18:46:49 +0100

    first commit

--- src/ValueGraphFromDataFrame.jl
+++ src/ValueGraphFromDataFrame.jl
@@ -1,5 +1,52 @@
 module ValueGraphFromDataFrame
 
-# Write your package code here.
+using SimpleValueGraphs
+using DataFrames
+
+export ValDiGraph
+
+function vattr_types_from_df(vertex_df)
+    vatypes = eltype.(eachcol(vertex_df))
+    vanames = names(vertex_df)
+    (; (Symbol.(vanames) .=> vatypes)...)
+end
+
+function make_edgelist_nodeidx(edgelist, vertex_df, vertex_df_col=1)
+    vind = DataFrame(name=vertex_df[:,vertex_df_col], idx=1:size(vertex_df, 1))
+    elnames = Symbol.(names(edgelist, [1,2]))
+    res = leftjoin(edgelist, vind, on=elnames[1] => :name)
+    rename!(res, :idx => :idx1)
+    res = leftjoin(res, vind, on=elnames[2] => :name)
+    select!(res, :idx1, :idx, Not(elnames))
+    rename!(res, :idx1 => elnames[1], :idx => elnames[2])
+    return res
+end
+
+function add_vertices_from_df!(vg, vertex_df)
+    for r in eachrow(vertex_df)
+       add_vertex!(vg, NamedTuple(r))
+    end
+end
+
+function add_edges_from_df!(vg, edgelist)
+    if size(edgelist, 2) > 2
+        for r in eachrow(edgelist)
+            add_edge!(vg, r[1], r[2], NamedTuple(r[Not([1,2])]))
+        end
+    else
+        for r in eachrow(edgelist)
+            add_edge!(vg, r[1], r[2])
+        end
+    end
+end
+
+function SimpleValueGraphs.ValDiGraph(edgelist::DataFrame, vertex_df::DataFrame; vertex_df_col=1)
+    vg = ValDiGraph(0, vertexval_types=vattr_types_from_df(vertex_df), vertexval_init=undef)
+    el = make_edgelist_nodeidx(edgelist, vertex_df, vertex_df_col)
+    add_vertices_from_df!(vg, vertex_df)
+    add_edges_from_df!(vg, el)
+    return vg
+end
+
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,30 @@
 using ValueGraphFromDataFrame
+using SimpleValueGraphs
+using LightGraphs
+using DataFrames
 using Test
 
+
+vertex_attributes = DataFrame(
+    name=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"], 
+    col1=1:10,
+    col2=vcat(trues(5), falses(5))
+)
+
+edgelist = DataFrame(
+    from=["a", "a", "a", "b", "f", "g", "d", "d", "c"],
+    to  =["b", "c", "i", "a", "d", "f", "e", "a", "j"],
+    #eval=1:9
+)
+
+g = ValDiGraph(edgelist, vertex_attributes)
+e = LightGraphs.edges(g) |> first
+
 @testset "ValueGraphFromDataFrame.jl" begin
-    # Write your tests here.
+    get_vertexval(g, 4, :name) == "d"
+    get_vertexval(g, 9, :col1) == 9
+    get_vertexval(g, 3, :col2) == true
+    get_vertexval(g, src(e), :name) == "a"
+    get_vertexval(g, dst(e), :col1) == 2
+    #get_edgeval(g, 1, 3, :eval) == 3
 end
